81a92ceac096f0ab1c90ed84624877c699fdfd95
Danilo25 <danilo25aciole@gmail.com>
2025-01-21 13:00:32 -0300

    Coletados_only_once

--- item/pick_ups/pick_up.gd
+++ item/pick_ups/pick_up.gd
@@ -3,6 +3,8 @@ extends RigidBody3D
 @export var slot_data: SlotData
 @onready var sprite_3d: Sprite3D = $Sprite3D
 
+signal collected
+
 func _ready() -> void:
 	sprite_3d.texture = slot_data.item_data.texture
 
@@ -10,6 +12,7 @@ func _physics_process(delta: float) -> void:
 	sprite_3d.rotate_y(delta)
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
+	collected.emit(self.position)
 	print("Colidiu")
 	if body.inventory_data.pick_up_slot_data(slot_data):
 		queue_free()
--- scripts/cave_entrance.gd
+++ scripts/cave_entrance.gd
@@ -3,6 +3,7 @@ class_name CaveEntrance
 
 var starting = false
 var cave_res = preload("res://scenes/cave_generator.tscn")
+var coletados = []
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is not Player or starting:
@@ -15,12 +16,22 @@ func _on_body_entered(body: Node3D) -> void:
 	var cave = cave_res.instantiate()
 	cave.seed = hash(global_position)
 	cave.entrance = self
+	cave.rng.seed = cave.seed
+	cave.initialize_grid()
+	cave.generate_cave()
+	cave.choose_start_position()
+	cave.save_pick_up_positions()
+	cave.coletados = coletados
 	main.add_child(cave)
+	cave.connect("collected", Callable(self, "coletadosadd"))
 	#cave.add_child(player)
 	#get_tree().change_scene_to_packed(cave)
 	#root.add_child(cave)
 	#cave.global_position = Vector3(global_position.x, global_position.y - 500, global_position.z)
 
+func coletadosadd(position: Vector2):
+	print("Coletou de dentro da caverna")
+	coletados.append(position)
 
 func _on_body_exited(body: Node3D) -> void:
 	if body is Player:
